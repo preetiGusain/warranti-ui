0cf25d4da86e80a46e4cdce767d27a3b7012ca40
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-12-17 18:28:02 +0530

    Divided create form in steps

--- src/pages/CreateWarranty.jsx
+++ src/pages/CreateWarranty.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, ProgressBar } from "react-bootstrap";
 import axios from "axios";
 import { backend_uri } from "../constants";
 import { useSearchParams } from "react-router-dom";
@@ -19,6 +19,7 @@ function CreateWarranty() {
 
     const [token, setToken] = useState(null);
     const [searchParams] = useSearchParams();
+    const [step, setStep] = useState(1);
 
     useEffect(() => {
         if (searchParams.has('token')) {
@@ -98,93 +99,100 @@ function CreateWarranty() {
         }
     }
 
+    function goToNext() {
+        setStep(step + 1);
+    }
+
     return (
-        <div style={{
-            padding: "20px", maxWidth: "600px", margin: "auto", border: "1px solid #ddd", borderRadius: "8px",
-            boxShadow: "0 2px 8px rgba(0,0,0,0.1)", height: "100vh", display: "flex", flexDirection: "column"
-        }}>
-            <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Create Warranty</h2>
+            <div className={`createContainer`}>
+                <h2 style={{ textAlign: "center", marginBottom: "20px", padding: "20px" }}>Create Warranty</h2>
+                {/* <h3 style={{ textAlign: "center", marginBottom: "20px" }}>Step {step} of 3</h3> */}
+                <ProgressBar animated variant="info" now={(step * 100) / 3} />
 
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="productName" style={{ marginBottom: "15px" }}>
-                    <Form.Label>Product Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        name="productName"
-                        value={formData.productName}
-                        onChange={handleInputChange}
-                        placeholder="Enter product name"
-                    />
-                </Form.Group>
+                <Form onSubmit={handleSubmit}>
 
-                <Form.Group controlId="warrantyDuration" style={{ marginBottom: "15px" }}>
-                    <Form.Label>Warranty Duration</Form.Label>
-                    <Form.Control
-                        type="number"
-                        name="warrantyDuration"
-                        value={formData.warrantyDuration}
-                        onChange={handleInputChange}
-                        placeholder="Enter warranty duration"
-                    />
-                </Form.Group>
+                    {step === 1 && <><Form.Group controlId="productName" style={{ marginBottom: "15px" }}>
+                        <Form.Label>Product Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="productName"
+                            value={formData.productName}
+                            onChange={handleInputChange}
+                            placeholder="Enter product name"
+                        />
+                    </Form.Group>
+                        <Form.Group controlId="product" style={{ marginBottom: "15px" }}>
+                            <Form.Label>Upload Product Image</Form.Label>
+                            <Form.Control
+                                type="file"
+                                accept="image/*"
+                                onChange={(e) => handleFileChange(e, "product")}
+                            />
+                        </Form.Group></>}
 
-                <Form.Group controlId="warrantyDurationUnit" style={{ marginBottom: "15px" }}>
-                    <Form.Label>Warranty Duration Unit</Form.Label>
-                    <Form.Control as="select"
-                        name="warrantyDurationUnit"
-                        value={formData.warrantyDurationUnit}
-                        onChange={handleInputChange}>
-                        <option value="Month">Month</option>
-                        <option value="Year">Year</option>
-                    </Form.Control>
-                </Form.Group>
+                    {step === 2 && <><Form.Group controlId="purchaseDate" style={{ marginBottom: "15px" }}>
+                        <Form.Label>Purchase Date</Form.Label>
+                        <Form.Control
+                            type="date"
+                            name="purchaseDate"
+                            value={formData.purchaseDate}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+                        <Form.Group controlId="receipt" style={{ marginBottom: "15px" }}>
+                            <Form.Label>Upload Receipt</Form.Label>
+                            <Form.Control
+                                type="file"
+                                accept="image/*"
+                                onChange={(e) => handleFileChange(e, "receipt")}
+                            />
+                        </Form.Group></>}
 
-                <Form.Group controlId="purchaseDate" style={{ marginBottom: "15px" }}>
-                    <Form.Label>Purchase Date</Form.Label>
-                    <Form.Control
-                        type="date"
-                        name="purchaseDate"
-                        value={formData.purchaseDate}
-                        onChange={handleInputChange}
-                    />
-                </Form.Group>
 
-                {/**Adding images */}
-                <Form.Group controlId="warrantyCard" style={{ marginBottom: "15px" }}>
-                    <Form.Label>Upload Warranty Card</Form.Label>
-                    <Form.Control
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => handleFileChange(e, "warrantyCard")}
-                    />
-                </Form.Group>
+                    {step === 3 && <><Form.Group controlId="warrantyDuration" style={{ marginBottom: "15px" }}>
+                        <Form.Label>Warranty Duration</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="warrantyDuration"
+                            value={formData.warrantyDuration}
+                            onChange={handleInputChange}
+                            placeholder="Enter warranty duration"
+                        />
+                    </Form.Group>
 
-                <Form.Group controlId="receipt" style={{ marginBottom: "15px" }}>
-                    <Form.Label>Upload Receipt</Form.Label>
-                    <Form.Control
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => handleFileChange(e, "receipt")}
-                    />
-                </Form.Group>
+                        <Form.Group controlId="warrantyDurationUnit" style={{ marginBottom: "15px" }}>
+                            <Form.Label>Warranty Duration Unit</Form.Label>
+                            <Form.Control as="select"
+                                name="warrantyDurationUnit"
+                                value={formData.warrantyDurationUnit}
+                                onChange={handleInputChange}>
+                                <option value="Month">Month</option>
+                                <option value="Year">Year</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="warrantyCard" style={{ marginBottom: "15px" }}>
+                            <Form.Label>Upload Warranty Card</Form.Label>
+                            <Form.Control
+                                type="file"
+                                accept="image/*"
+                                onChange={(e) => handleFileChange(e, "warrantyCard")}
+                            />
+                        </Form.Group></>}
+                    {step === 3 && <Button
+                        variant="primary"
+                        type="submit"
+                        style={{ marginTop: "20px", width: "100%" }}
+                    >Submit
+                    </Button>}
+                    {(step === 1 || step === 2) && <Button
+                        variant="primary"
+                        style={{ marginTop: "20px", width: "100%" }}
+                        onClick={goToNext}
+                    >Next
+                    </Button>}
 
-                <Form.Group controlId="product" style={{ marginBottom: "15px" }}>
-                    <Form.Label>Upload Product Image</Form.Label>
-                    <Form.Control
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => handleFileChange(e, "product")}
-                    />
-                </Form.Group>
-
-                <Button
-                    variant="primary"
-                    type="submit"
-                    style={{ marginTop: "20px", width: "100%" }}
-                >Submit Warranty
-                </Button>
-            </Form>
-        </div>
+                </Form>
+            </div>
     )
 }
 
